f4f02e502c9d09c5f661ce7586e016600dea4871
rafael <rafaelmagalhaes1@live.com>
2024-03-03 00:29:07 -0300

    initial commit

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -11,7 +11,7 @@ class App {
 
     this.config();
 
-    // Não remover essa rota
+    // Não remover essa rota!
     this.app.get('/', (req, res) => res.json({ ok: true }));
 
     // Não remova esse middleware de erro, mas fique a vontade para customizá-lo
