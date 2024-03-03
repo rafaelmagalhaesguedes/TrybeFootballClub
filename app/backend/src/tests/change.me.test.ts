5f418a5622f77778b3f2929098085b3a4e15f69e
rafael <rafaelmagalhaes1@live.com>
2024-03-03 17:35:42 -0300

    tests: add tests of integration to findById method

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -12,12 +12,48 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Teams test', () => {
-  it('should return all teams', async function() {
-    sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+  afterEach(() => {
+    sinon.restore();
+  });
 
-    const { status, body } = await chai.request(app).get('/teams');
+  describe('1. Get All teams', () => {
+    it('should return all teams', async function() {
+      // arrange
+      sinon.stub(SequelizeTeam, 'findAll').resolves(teams as any);
+  
+      // act
+      const { status, body } = await chai.request(app).get('/teams');
+  
+      // assert
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(teams);
+    });
+  });
 
-    expect(status).to.equal(200);
-    expect(body).to.deep.equal(teams);
+  describe('2. Get Team by id', () => {
+    it('should return a team by id', async function() {
+      // arrange
+      const team = teams[0];
+      sinon.stub(SequelizeTeam, 'findByPk').resolves(team as any);
+
+      // act
+      const { status, body } = await chai.request(app).get(`/teams/${team.id}`);
+
+      // assert
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(team);
+    });
+
+    it('should return 404 if team is not found', async function() {
+      // arrange
+      sinon.stub(SequelizeTeam, 'findByPk').resolves(null);
+
+      // act
+      const { status, body } = await chai.request(app).get('/teams/1');
+
+      // assert
+      expect(status).to.equal(404);
+      expect(body).to.deep.equal({ message: 'Team not found' });
+    });
   });
 });
