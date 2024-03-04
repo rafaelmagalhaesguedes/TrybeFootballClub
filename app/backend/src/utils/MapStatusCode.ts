346f73779d383b11bc824a61693a7dc8c7781f2f
rafael <rafaelmagalhaes1@live.com>
2024-03-04 09:38:24 -0300

    fix: add status code

--- app/backend/src/utils/MapStatusCode.ts
+++ app/backend/src/utils/MapStatusCode.ts
@@ -1,8 +1,9 @@
 //
-export default function mapStatusHTTP(status: string): number {
+export default function statusCode(status: string): number {
   switch (status) {
     case 'SUCCESSFUL': return 200;
     case 'INVALID_DATA': return 400;
+    case 'UNAUTHORIZED': return 401;
     case 'NOT_FOUND': return 404;
     case 'CONFLICT': return 409;
     default: return 500;
