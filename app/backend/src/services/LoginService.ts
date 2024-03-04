6cd039bb482fde5402ca1e999d556eba00d35d82
rafael <rafaelmagalhaes1@live.com>
2024-03-04 10:03:50 -0300

    fix: constants info message on LoginService and JwtService

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -6,18 +6,18 @@ import IUserModel from '../Interfaces/User/IUserModel';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
 import IUser from '../Interfaces/User/IUser';
 
-const errorMessage = 'Invalid email or password';
-
 export default class LoginService {
   //
+  private static errorMessage = 'Invalid email or password';
+
   constructor(private userModel: IUserModel = new UserModel()) { }
 
   async sign(email: string, password: string): Promise<ServiceResponse<IUser | string>> {
     const user = await this.userModel.findUserByEmail(email);
-    if (!user) return { status: 'UNAUTHORIZED', data: { message: errorMessage } };
+    if (!user) return { status: 'UNAUTHORIZED', data: { message: LoginService.errorMessage } };
 
     if (!bcript.compareSync(password, user.password)) {
-      return { status: 'UNAUTHORIZED', data: { message: errorMessage } };
+      return { status: 'UNAUTHORIZED', data: { message: LoginService.errorMessage } };
     }
 
     const token = JwtService.createToken({ email: user.email, role: user.role });
--- app/backend/src/utils/JwtService.ts
+++ app/backend/src/utils/JwtService.ts
@@ -1,20 +1,21 @@
 import * as jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'secretKey';
-
 export default class JwtService {
+  //
+  private static jwtSecret = process.env.JWT_SECRET || 'secretKey';
+
   static createToken({ email, role }: { email: string; role: string }) {
     const jwtConfig: jwt.SignOptions = {
       expiresIn: '1h',
       algorithm: 'HS256',
     };
 
-    const token = jwt.sign({ email, role }, JWT_SECRET, jwtConfig);
+    const token = jwt.sign({ email, role }, JwtService.jwtSecret, jwtConfig);
 
     return token;
   }
 
   static verify(token: string) {
-    return jwt.verify(token, JWT_SECRET);
+    return jwt.verify(token, JwtService.jwtSecret);
   }
 }
