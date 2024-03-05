3754eba5a7ff8ae4feb30c5d76fb3ccc700c4b21
rafael <rafaelmagalhaes1@live.com>
2024-03-05 10:14:44 -0300

    refact: LoginMiddlware validations

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -2,33 +2,33 @@ import * as Joi from 'joi';
 import { NextFunction, Request, Response } from 'express';
 import IUser from '../Interfaces/User/IUser';
 
-export default class LoginValidations {
+export default class LoginMiddleware {
   private static messageEmptyField = 'All fields must be filled';
   private static messageInvalidField = 'Invalid email or password';
 
   private static loginSchema = Joi.object({
     email: Joi.string().email().required().messages({
-      'string.email': LoginValidations.messageInvalidField,
-      'string.empty': LoginValidations.messageEmptyField,
-      'any.required': LoginValidations.messageEmptyField,
+      'string.email': LoginMiddleware.messageInvalidField,
+      'string.empty': LoginMiddleware.messageEmptyField,
+      'any.required': LoginMiddleware.messageEmptyField,
     }),
     password: Joi.string().min(6).required().messages({
-      'string.min': LoginValidations.messageInvalidField,
-      'string.empty': LoginValidations.messageEmptyField,
-      'any.required': LoginValidations.messageEmptyField,
+      'string.min': LoginMiddleware.messageInvalidField,
+      'string.empty': LoginMiddleware.messageEmptyField,
+      'any.required': LoginMiddleware.messageEmptyField,
     }),
   });
 
   private static validateLoginFields(body: IUser) {
-    const { error } = LoginValidations.loginSchema.validate(body);
+    const { error } = LoginMiddleware.loginSchema.validate(body);
     if (error) return error.details[0].message;
   }
 
   static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
-    const message = LoginValidations.validateLoginFields(req.body);
+    const message = LoginMiddleware.validateLoginFields(req.body);
 
     if (message) {
-      if (message === LoginValidations.messageInvalidField) {
+      if (message === LoginMiddleware.messageInvalidField) {
         return res.status(401).json({ message });
       }
       return res.status(400).json({ message });
