70f08a7be9a02cdb8b4838438cec6824452084c2
rafael <rafaelmagalhaes1@live.com>
2024-03-05 10:13:33 -0300

    fix: spacing in the code

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,6 +1,4 @@
 //
-import { ITeam } from '../Interfaces/Team/ITeam';
-import { ServiceResponse } from '../Interfaces/ServiceResponse';
 import { ITeamModel } from '../Interfaces/Team/ITeamModel';
 import TeamModel from '../models/TeamModel';
 
@@ -8,14 +6,17 @@ export default class TeamService {
   //
   constructor(private teamModel: ITeamModel = new TeamModel()) { }
 
-  public async getAllTeams(): Promise<ServiceResponse<ITeam[]>> {
+  async getAllTeams() {
     const allTeams = await this.teamModel.findAll();
+    if (!allTeams) return { status: 'NOT_FOUND', data: { message: 'Teams not found' } };
+
     return { status: 'SUCCESSFUL', data: allTeams };
   }
 
-  public async getTeamById(id: number): Promise<ServiceResponse<ITeam | null>> {
+  async getTeamById(id: number) {
     const team = await this.teamModel.findById(id);
     if (!team) return { status: 'NOT_FOUND', data: { message: 'Team not found' } };
+
     return { status: 'SUCCESSFUL', data: team };
   }
 }
