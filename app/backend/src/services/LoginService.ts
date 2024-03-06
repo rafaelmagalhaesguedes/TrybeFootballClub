98d280be97515eed2d3ea7249976832662731f20
rafael <rafaelmagalhaes1@live.com>
2024-03-06 10:03:42 -0300

    refact: LoginService on services layer

--- app/backend/src/services/LoginService.ts
+++ app/backend/src/services/LoginService.ts
@@ -2,7 +2,9 @@
 import * as bcript from 'bcryptjs';
 import JwtService from '../utils/JwtService';
 import UserModel from '../models/UserModel';
-import IUserModel from '../Interfaces/User/IUserModel';
+import { IUserModel } from '../Interfaces/User/IUserModel';
+import { ServiceResponse, ServiceMessage } from '../Interfaces/ServiceResponse';
+import { ILogin, IUserRole, IToken } from '../Interfaces/User/IUser';
 
 export default class LoginService {
   //
@@ -10,23 +12,29 @@ export default class LoginService {
 
   constructor(private userModel: IUserModel = new UserModel()) { }
 
-  public async sign(email: string, password: string) {
-    const user = await this.userModel.findUserByEmail(email);
+  public async sign(data: ILogin): Promise<ServiceResponse<ServiceMessage | IToken>> {
+    // Check if user exists
+    const user = await this.userModel.findUserByEmail(data.email);
     if (!user) return { status: 'UNAUTHORIZED', data: { message: this.errorMessage } };
 
-    if (!bcript.compareSync(password, user.password)) {
+    // Check if password is correct
+    if (!bcript.compareSync(data.password, user.password)) {
       return { status: 'UNAUTHORIZED', data: { message: this.errorMessage } };
     }
 
+    // Create token
     const token = JwtService.createToken({ id: user.id, email: user.email });
 
+    // Return token
     return { status: 'SUCCESSFUL', data: { token } };
   }
 
-  public async getRole(email: string) {
+  public async getRole(email: string): Promise<ServiceResponse<ServiceMessage | IUserRole>> {
+    // Check if user exists
     const user = await this.userModel.findUserByEmail(email);
     if (!user) return { status: 'UNAUTHORIZED', data: { message: this.errorMessage } };
 
+    // Return user role
     return { status: 'SUCCESSFUL', data: { role: user.role } };
   }
 }
