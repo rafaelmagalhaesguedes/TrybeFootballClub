d8e698a00c5a8fbc6b4ef3270190c2c834601c46
rafael <rafaelmagalhaes1@live.com>
2024-03-06 13:59:19 -0300

    feat: findAllMatches method created on MatchesModel

--- app/backend/src/models/MatchesModel.ts
+++ app/backend/src/models/MatchesModel.ts
@@ -1,22 +1,23 @@
 //
 import SequelizeMatches from '../database/models/SequelizeMatches';
 import { IMatchesModel } from '../Interfaces/Matches/IMatchesModel';
-import SequelizeTeams from '../database/models/SequelizeTeams';
+import SequelizeTeams from '../database/models/SequelizeTeam';
 import { IMatches } from '../Interfaces/Matches/IMatches';
 
 export default class MatchesModel implements IMatchesModel {
-  private model = SequelizeMatches;
+  private matchesModel = SequelizeMatches;
+  private teamModel = SequelizeTeams;
 
-  public async getMatches(): Promise<IMatches[] | null> {
-    const result = await this.model.findAll({
+  public async findAllMatches(): Promise<IMatches[] | null> {
+    const matches = await this.matchesModel.findAll({
       include: [
-        { model: SequelizeTeams, as: 'homeTeam', attributes: ['teamName'] },
-        { model: SequelizeTeams, as: 'awayTeam', attributes: ['teamName'] },
+        { model: this.teamModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: this.teamModel, as: 'awayTeam', attributes: ['teamName'] },
       ],
     });
 
-    if (result.length === 0) return null;
+    if (matches.length === 0) return null;
 
-    return result;
+    return matches;
   }
 }
