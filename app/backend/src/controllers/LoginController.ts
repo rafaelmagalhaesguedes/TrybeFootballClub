119ceaf00ac72de408ed5eb63e271879b7d967ad
rafael <rafaelmagalhaes1@live.com>
2024-03-06 10:03:58 -0300

    refact: LoginController on controllers layer

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -7,13 +7,12 @@ export default class LoginController {
   //
   constructor(private loginService = new LoginService()) { }
 
-  public async signUp(req: Request, res: Response) {
-    const { email, password } = req.body;
-    const { status, data } = await this.loginService.sign(email, password);
+  public async signUp(req: Request, res: Response): Promise<Response> {
+    const { status, data } = await this.loginService.sign(req.body);
     return res.status(statusCode(status)).json(data);
   }
 
-  public async getUserRole(_req: Request, res: Response) {
+  public async getUserRole(_req: Request, res: Response): Promise<Response> {
     const { email } = res.locals.user;
     const { status, data } = await this.loginService.getRole(email);
     return res.status(statusCode(status)).json(data);
