108a2af96df3e233bad4d208c3ffba4618b9fc17
rafael <rafaelmagalhaes1@live.com>
2024-03-06 10:03:27 -0300

    refact: UserModel on models layer

--- app/backend/src/models/UserModel.ts
+++ app/backend/src/models/UserModel.ts
@@ -1,14 +1,21 @@
-//
 import IUser from '../Interfaces/User/IUser';
-import IUserModel from '../Interfaces/User/IUserModel';
+import { IUserModel } from '../Interfaces/User/IUserModel';
 import SequelizeUser from '../database/models/SequelizeUser';
 
 export default class UserModel implements IUserModel {
   private model = SequelizeUser;
 
-  async findUserByEmail(email: string): Promise<IUser | null> {
+  public async findUserByEmail(email: IUser['email']): Promise<IUser | null> {
+    // Find user by email
     const user = await this.model.findOne({ where: { email } });
+
+    // If user does not exist, return null
     if (!user) return null;
-    return user;
+
+    // Destructure user
+    const { id, username, role, password } = user;
+
+    // Return user
+    return { id, username, email, role, password };
   }
 }
