59675bedd0176f4cba27b116709148f639168767
rafael <rafaelmagalhaes1@live.com>
2024-03-07 12:31:01 -0300

    feat: updateMatch created on IMatchesModel interface

--- app/backend/src/Interfaces/Matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/Matches/IMatchesModel.ts
@@ -2,4 +2,5 @@ import { IMatches } from './IMatches';
 
 export interface IMatchesModel {
   findMatches(query?: boolean | undefined): Promise<IMatches[] | null>;
+  updateMatch(id: number): Promise<IMatches | null>;
 }
