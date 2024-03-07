4834c8d18978c86fa5e325ed15e31c0612acc649
rafael <rafaelmagalhaes1@live.com>
2024-03-07 18:42:48 -0300

    tests: integration tests created to update Match progress and update Match results

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -4,26 +4,41 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import SequelizeMatches from '../database/models/SequelizeMatches';
+import MatchesModel from '../database/models/SequelizeMatches';
 import { matchesMock } from './mocks/matches.mocks';
+import * as jwt from 'jsonwebtoken';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
+// Generates a new token
+const secret = 'secretKey';
+const payload = { userId: 1, userEmail: 'rafael@dev.com'};
+const options = { expiresIn: '1h' };
+
+const generateToken = () => jwt.sign(payload, secret, options);
+
+let tokenMock: string;
+
 describe('Matches Tests', () => {
+
+  beforeEach(() => {
+    tokenMock = generateToken();
+  });
+
   afterEach(() => {
     sinon.restore();
   });
-    
+
   describe('Integration tests', () => {
     it('should return all matches', async () => {
       // arrange
-      sinon.stub(SequelizeMatches, 'findAll').resolves(matchesMock as any);
-    
+      sinon.stub(MatchesModel, 'findAll').resolves(matchesMock as any);
+
       // act
       const res = await chai.request(app).get('/matches');
-    
+
       // assert
       expect(res.status).to.equal(200);
       expect(res.body).to.deep.equal(matchesMock);
@@ -31,14 +46,48 @@ describe('Matches Tests', () => {
 
     it('should return all matches in progress', async () => {
       // arrange
-      sinon.stub(SequelizeMatches, 'findAll').resolves(matchesMock as any);
-    
+      const inProgressMatches = matchesMock.filter(match => match.inProgress);
+      sinon.stub(MatchesModel, 'findAll').resolves(inProgressMatches as any);
+
       // act
       const res = await chai.request(app).get('/matches?inProgress=true');
+
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal(inProgressMatches);
+    });
+
+    it('should update a match', async () => {
+      // arrange
+      const matchId = 1;
+      const updatedMatch = { ...matchesMock[0], inProgress: false };
+      sinon.stub(MatchesModel.prototype, 'update').resolves(updatedMatch as any);
+    
+      // act
+      const res = await chai.request(app)
+        .patch(`/matches/${matchId}/finish`)
+        .set('Authorization', `Bearer ${tokenMock}`);
     
       // assert
       expect(res.status).to.equal(200);
-      expect(res.body).to.deep.equal(matchesMock);
+      expect(res.body).to.deep.equal({ message: 'Finished' });
+    });
+
+    it('should update match results', async () => {
+      // arrange
+      const matchId = 1;
+      const updatedMatch = { ...matchesMock[0], homeTeamGoals: 2, awayTeamGoals: 1 };
+      sinon.stub(MatchesModel, 'update').resolves(updatedMatch as any);
+
+      // act
+      const res = await chai.request(app)
+        .patch(`/matches/${matchId}`)
+        .set('Authorization', `Bearer ${tokenMock}`)
+        .send({ homeTeamGoals: 6, awayTeamGoals: 1 });
+
+      // assert
+      expect(res.status).to.equal(200);
+      expect(res.body).to.deep.equal({ message: 'The match result has been changed' });
     });
   });
 });
