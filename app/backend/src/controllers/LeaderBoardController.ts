cd1444b53dbcc857c0d1a0a228fe27f3e3f4c805
rafael <rafaelmagalhaes1@live.com>
2024-03-08 13:49:05 -0300

    feat: LeaderBoardController created

---
+++ app/backend/src/controllers/LeaderBoardController.ts
@@ -0,0 +1,19 @@
+//
+import { Request, Response } from 'express';
+import LeaderService from '../services/LeaderBoardService';
+import statusCode from '../utils/MapStatusCode';
+
+export default class LeaderController {
+  //
+  constructor(private leaderService = new LeaderService()) { }
+
+  public async leaderBoardHome(_req: Request, res: Response) {
+    const { status, data } = await this.leaderService.getHomeLeaderBoard();
+    res.status(statusCode(status)).json(data);
+  }
+
+  public async leaderBoardAway(_req: Request, res: Response) {
+    const { status, data } = await this.leaderService.getAwayLeaderBoard();
+    res.status(statusCode(status)).json(data);
+  }
+}
