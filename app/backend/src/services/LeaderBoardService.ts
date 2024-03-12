404523b81740820e3a64870c60b20b31fc7f050c
rafael <rafaelmagalhaes1@live.com>
2024-03-12 10:04:30 -0300

    refact: LeaderBoardService refactored

--- app/backend/src/services/LeaderBoardService.ts
+++ app/backend/src/services/LeaderBoardService.ts
@@ -1,76 +1,67 @@
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
 import { ILeaderBoard } from '../Interfaces/LeaderBoard/ILeaderBoard';
-import teamsClassified from '../utils/SortMatches';
+import Sort from '../utils/SortMatches';
 import TeamData from '../LeaderBoard/TeamData';
-import SequelizeMatches from '../database/models/SequelizeMatches';
-import SequelizeTeam from '../database/models/SequelizeTeam';
+/* import SequelizeMatches from '../database/models/SequelizeMatches'; */
+/* import SequelizeTeam from '../database/models/SequelizeTeam'; */
+import TeamModel from '../models/TeamModel';
+import MatchesModel from '../models/MatchesModel';
 
 export default class LeaderBoardService {
   //
-  private matchesModel = SequelizeMatches;
-  private teamModel = SequelizeTeam;
-  private teamBoard = new TeamData();
+  constructor(
+    private matchesModel = new MatchesModel(),
+    private teamModel = new TeamModel(),
+    private teamData = new TeamData(),
+  ) { }
 
   public async getLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
-    const getTeams = await this.teamModel.findAll();
+    const allTeams = await this.teamModel.getHomeAndAwayTeams();
 
-    const teams = getTeams.map(async (team) => {
-      const homeMatches = await this.matchesModel.findAll({
-        where: { homeTeamId: team.id, inProgress: false } });
-
-      const awayMatches = await this.matchesModel.findAll({
-        where: { awayTeamId: team.id, inProgress: false } });
-
-      const teamsStats = this.teamBoard.getAllTeamsData(team.teamName, homeMatches, awayMatches);
+    const teams = allTeams.map(async (team) => {
+      const { teamName, homeMatches, awayMatches } = team;
+      const teamsStats = this.teamData
+        .getAllTeamsData(teamName, homeMatches || [], awayMatches || []);
       return { ...teamsStats };
     });
 
     const results = await Promise.all(teams);
-    const orderdResults = teamsClassified.sortMatches(results);
-
-    return { status: 'SUCCESSFUL', data: orderdResults };
+    const orderedResults = Sort(results);
+    return { status: 'SUCCESSFUL', data: orderedResults };
   }
 
   public async getHomeLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
-    const getTeams = await this.teamModel.findAll();
+    const allTeams = await this.teamModel.getHomeAndAwayTeams();
 
-    const homeTeams = getTeams.map(async (team) => {
-      const homeMatches = await this.matchesModel.findAll({
-        where: { homeTeamId: team.id, inProgress: false } });
-
+    const homeTeams = allTeams.map(async (team) => {
+      const homeMatches = await this.matchesModel.getHomeMatches(team.id);
       const homeStats = homeMatches.map((match) =>
-        this.teamBoard.getTeamData(team.teamName, [match], true));
-
+        this.teamData.getTeamData(team.teamName, [match], true));
       const teamsStats = homeStats[homeMatches.length - 1];
       return { ...teamsStats };
     });
 
     const results = await Promise.all(homeTeams);
-    const orderdResults = teamsClassified.sortMatches(results);
-
+    const orderdResults = Sort(results);
     return { status: 'SUCCESSFUL', data: orderdResults };
   }
 
   public async getAwayLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
-    const getTeams = await this.teamModel.findAll();
+    const allTeams = await this.teamModel.getHomeAndAwayTeams();
 
-    const awayTeams = getTeams.map(async (team) => {
-      const awayMatches = await this.matchesModel.findAll({
-        where: { awayTeamId: team.id, inProgress: false } });
-
+    const awayTeams = allTeams.map(async (team) => {
+      const awayMatches = await this.matchesModel.getAwayMatches(team.id);
       const awayStats = awayMatches.map((match) =>
-        this.teamBoard.getTeamData(team.teamName, [match], false));
-
+        this.teamData.getTeamData(team.teamName, [match], false));
       const teamsStats = awayStats[awayMatches.length - 1];
       return { ...teamsStats };
     });
 
     const results = await Promise.all(awayTeams);
-    const orderdResults = teamsClassified.sortMatches(results);
-
+    const orderdResults = Sort(results);
     return { status: 'SUCCESSFUL', data: orderdResults };
   }
 }
