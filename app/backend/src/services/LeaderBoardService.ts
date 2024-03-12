bef0fdc6bfbf36eef6b3b86ea9bc7418006db9cf
rafael <rafaelmagalhaes1@live.com>
2024-03-12 12:43:23 -0300

    fix: getLeaderBoard method

--- app/backend/src/services/LeaderBoardService.ts
+++ app/backend/src/services/LeaderBoardService.ts
@@ -17,22 +17,21 @@ export default class LeaderBoardService {
   public async getLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
     const getAllTeams = await this.teamModel.getHomeAndAwayTeams();
-
     const filterTeams = getAllTeams.map(async (team) => {
       const { teamName, homeMatches, awayMatches } = team;
-      const updateStats = this.teamData.getAllTeams(teamName, homeMatches || [], awayMatches || []);
+      if (!homeMatches || !awayMatches) return { status: 'FAILED', message: 'No matches found!' };
+      const updateStats = this.teamData.getAllTeams(teamName, homeMatches, awayMatches);
       return { ...updateStats };
     });
 
     const allTeams = await Promise.all(filterTeams);
-    const orderedTeams = Sort(allTeams);
+    const orderedTeams = Sort(allTeams as ILeaderBoard[]);
     return { status: 'SUCCESSFUL', data: orderedTeams };
   }
 
   public async getHomeLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
     const getAllTeams = await this.teamModel.getHomeAndAwayTeams();
-
     const filterHomeTeams = getAllTeams.map(async (team) => {
       const homeMatches = await this.matchesModel.getHomeMatches(team.id);
       const updateStats = this.teamData.getTeam(team.teamName, homeMatches, true);
@@ -47,7 +46,6 @@ export default class LeaderBoardService {
   public async getAwayLeaderBoard(): Promise<ServiceResponse<ILeaderBoard[]>> {
     //
     const getAllTeams = await this.teamModel.getHomeAndAwayTeams();
-
     const filterAwayTeams = getAllTeams.map(async (team) => {
       const awayMatches = await this.matchesModel.getAwayMatches(team.id);
       const updateStats = this.teamData.getTeam(team.teamName, awayMatches, false);
